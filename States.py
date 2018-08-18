975f4d0697122ac6d728d4d4e4ecb21d9bf563ed
jeroen11dijk <jeroen11dijk@gmail.com>
2018-08-18 22:25:22 +0200

    Initial commit

---
+++ States.py
@@ -0,0 +1,108 @@
+import math
+import time
+from Util import *
+from rlbot.agents.base_agent import  SimpleControllerState
+
+class boostManager:
+    def __init__(self):
+        self.expired = False
+
+    def available(self,agent):
+        pad = getClosestPad(agent)
+        distance = distance2D(agent.deevo, pad)
+        if distance < 2500 and agent.deevo.boost < 34 and boostAvailable(agent, pad):
+            return True
+        return False
+
+    def execute(self, agent):
+        print("GRABBING BOOST")
+        agent.controller = calcController
+        targetLocation = getClosestPad(agent)
+        boostAvailable(agent, targetLocation)
+        targetLocal = toLocal(targetLocation,agent.deevo)
+        angleToTarget = math.atan2(targetLocal.data[1], targetLocal.data[0])
+        distanceToTarget = distance2D(agent.deevo, targetLocation)
+        speedCorrection =  ((1+ abs(angleToTarget)**2) * 300)
+        speed = 1399 - speedCorrection + cap((distanceToTarget/16)**2,0,speedCorrection)
+
+        if agent.deevo.boost > 90 or not(boostAvailable(agent, targetLocation)):
+            self.expired = True
+
+        return agent.controller(agent, targetLocation, speed)
+
+class defending:
+    def __init__(self):
+        self.expired = False
+
+    def execute(self,agent):
+        print("DEFENDING")
+        agent.controller = calcController
+        goal = agent.ourGoal
+        goalToBall = (agent.ball.location - goal)
+        targetVector = Vector3([1/2 * goalToBall.data[0], 1/2 * goalToBall.data[1], 0])
+        targetLocation = goal + targetVector
+        targetLocation.data[0] = cap(targetLocation.data[0],-4120,4120)
+        if sign(agent.team) == 1:
+            targetLocation.data[1] = cap(targetLocation.data[1],0, 5100)
+        elif sign(agent.team) == -1:
+            targetLocation.data[1] = cap(targetLocation.data[1],-5100, 0)
+
+        targetLocal = toLocal(targetLocation,agent.deevo)
+        angleToTarget = math.atan2(targetLocal.data[1], targetLocal.data[0])
+        distanceToTarget = distance2D(agent.deevo, targetLocation)
+        speedCorrection =  ((1+ abs(angleToTarget)**2) * 300)
+        speed = 2300 - speedCorrection + cap((distanceToTarget/16)**2,0,speedCorrection)
+        self.expired = True
+
+        return agent.controller(agent,targetLocation,speed)
+
+class calcShot:
+    def __init__(self):
+        self.expired = False
+
+    def available(self,agent):
+        if (ballReady(agent) and ballProject(agent) > 500) or distance2D(agent.ball, agent.ourGoal) < 3000:
+            return True
+        return False
+
+    def execute(self,agent):
+        print("CALCULATE")
+        agent.controller = calcController
+        goal = agent.theirGoal
+        goalToBall = (agent.ball.location - goal).normalize()
+
+        goalToAgent = (agent.deevo.location - goal).normalize()
+        difference = goalToBall - goalToAgent
+        error = cap(abs(difference.data[0]) + abs(difference.data[1]),1,10)
+
+        targetDistance = (100 +distance2D(agent.ball,agent.deevo) * (error**2) )/ 1.95
+        targetLocation = agent.ball.location + Vector3([goalToBall.data[0]*targetDistance,goalToBall.data[1]*targetDistance, 0])
+        targetLocation.data[0] = cap(targetLocation.data[0],-4120,4120)
+
+        targetLocal = toLocal(targetLocation,agent.deevo)
+        angleToTarget = math.atan2(targetLocal.data[1], targetLocal.data[0])
+        distanceToTarget = distance2D(agent.deevo, targetLocation)
+        speedCorrection =  ((1+ abs(angleToTarget)**2) * 300)
+        speed = 2300 - speedCorrection + cap((distanceToTarget/16)**2,0,speedCorrection)
+
+        if ballProject(agent) < 10:
+            self.expired = True
+
+        return agent.controller(agent,targetLocation,speed)
+
+def calcController(agent, targetObject, targetSpeed):
+    location = toLocal(targetObject,agent.deevo)
+    controllerState = SimpleControllerState()
+    angleToBall = math.atan2(location.data[1],location.data[0])
+
+    currentSpeed = velocity2D(agent.deevo)
+    controllerState.steer = steer(angleToBall)
+
+    #throttle
+    if targetSpeed > currentSpeed:
+        controllerState.throttle = 1.0
+        if targetSpeed > 1400 and agent.start > 2.2 and currentSpeed < 2250:
+            controllerState.boost = True
+    elif targetSpeed < currentSpeed:
+        controllerState.throttle = -1.0
+    return controllerState
---
+++ Util.py
@@ -0,0 +1,161 @@
+import math
+
+GOAL_WIDTH = 1784
+FIELD_LENGTH = 10240
+FIELD_WIDTH = 8192
+
+class Vector3:
+    def __init__(self, data):
+        self.data = data
+    def __add__(self,value):
+        return Vector3([self.data[0]+value.data[0],self.data[1]+value.data[1],self.data[2]+value.data[2]])
+    def __sub__(self,value):
+        return Vector3([self.data[0]-value.data[0],self.data[1]-value.data[1],self.data[2]-value.data[2]])
+    def __mul__(self,value):
+        return (self.data[0]*value.data[0] + self.data[1]*value.data[1] + self.data[2]*value.data[2])
+    def __str__(self):
+        return str(self.data)
+    def length(self):
+        return math.sqrt(self*self)
+    def normalize(self):
+        length = self.length()
+        return Vector3([self.data[0]/length,self.data[1]/length,self.data[2]/length])
+
+class obj:
+    def __init__(self):
+        self.location = Vector3([0,0,0])
+        self.velocity = Vector3([0,0,0])
+        self.rotation = Vector3([0,0,0])
+        self.rotationVelocity = Vector3([0,0,0])
+
+        self.localLocation = Vector3([0,0,0])
+        self.boost = 0
+
+def to_local(targetObject, ourObject):
+    x = (toLocation(targetObject) - ourObject.location) * ourObject.matrix[0]
+    y = (toLocation(targetObject) - ourObject.location) * ourObject.matrix[1]
+    z = (toLocation(targetObject) - ourObject.location) * ourObject.matrix[2]
+    return Vector3([x,y,z])
+
+def getBigBoostPads(self):
+    res = []
+    for i in range(len(self.fieldInfo.boost_pads)):
+        current = self.fieldInfo.boost_pads[i]
+        if current.is_full_boost:
+            newVector = convertVector3(current.location)
+            res.append(newVector)
+    return res
+
+def boostAvailable(agent, pad):
+    index = -1
+    for i in range(len(agent.fieldInfo.boost_pads)):
+        if distance2D(convertVector3(agent.fieldInfo.boost_pads[i].location),pad) < 1:
+            index = i
+    return agent.boosts[index].is_active
+
+def getAttackingGoal(self):
+    res = []
+    for i in range(len(self.fieldInfo.goals)):
+        current = self.fieldInfo.goals[i]
+        if(sign(current.team_num) == -sign(self.team)):
+            leftPost = Vector3([sign(self.team)*GOAL_WIDTH/2, current.location.y, current.location.z])
+            rightPost = Vector3([-sign(self.team)*GOAL_WIDTH/2, current.location.y, current.location.z])
+            res.append(leftPost)
+            res.append(rightPost)
+    return res
+
+def getOurGoal(self):
+    for i in range(len(self.fieldInfo.goals)):
+        current = self.fieldInfo.goals[i]
+        if(sign(current.team_num) == sign(self.team)):
+            return convertVector3(current.location)
+
+def getTheirGoal(self):
+    for i in range(len(self.fieldInfo.goals)):
+        current = self.fieldInfo.goals[i]
+        if(sign(current.team_num) != sign(self.team)):
+            return convertVector3(current.location)
+
+def rotator_to_matrix(ourObject):
+    r = ourObject.rotation.data
+    CR = math.cos(r[2])
+    SR = math.sin(r[2])
+    CP = math.cos(r[0])
+    SP = math.sin(r[0])
+    CY = math.cos(r[1])
+    SY = math.sin(r[1])
+
+    matrix = []
+    matrix.append(Vector3([CP*CY, CP*SY, SP]))
+    matrix.append(Vector3([CY*SP*SR-CR*SY, SY*SP*SR+CR*CY, -CP * SR]))
+    matrix.append(Vector3([-CR*CY*SP-SR*SY, -CR*SY*SP+SR*CY, CP*CR]))
+    return matrix
+
+def sign(x):
+    if x <= 0:
+        return -1
+    else:
+        return 1
+
+def convertVector3(vector):
+    return Vector3([vector.x, vector.y, vector.z])
+
+def getClosestPad(agent):
+    pads = agent.bigBoostPads
+    closestPad = None
+    distToClosestPad = math.inf
+    for i in range(len(pads)):
+        if(distance2D(agent.deevo, pads[i]) < distToClosestPad):
+            distToClosestPad = distance2D(agent.deevo, pads[i])
+            closestPad = pads[i]
+    return closestPad
+
+def angle2D(targetLocation, objectLocation):
+    difference = toLocation(targetLocation) - toLocation(objectLocation)
+    return math.atan2(difference.data[1], difference.data[0])
+
+def velocity2D(targetObject):
+    return math.sqrt(targetObject.velocity.data[0]**2 + targetObject.velocity.data[1]**2)
+
+def toLocal(target, ourObject):
+    if isinstance(target, obj):
+        return target.localLocation
+    else:
+        return to_local(target, ourObject)
+
+def ballReady(agent):
+    ball = agent.ball
+    if abs(ball.velocity.data[2]) < 100 and ball.location.data[2] < 250:
+        if abs(ball.location.data[1]) < 5000:
+            return True
+    return False
+
+def ballProject(agent):
+    goal = agent.theirGoal
+    goalToBall = (agent.ball.location - goal).normalize()
+    diff = agent.deevo.location - agent.ball.location
+    return diff * goalToBall
+
+def cap(x, low, high):
+    if x < low:
+        return low
+    elif x > high:
+        return high
+    else:
+        return x
+
+def steer(angle):
+    final = ((10 * angle + sign(angle))**3) / 20
+    return cap(final,-1,1)
+
+def toLocation(target):
+    if isinstance(target, Vector3):
+        return target
+    elif isinstance(target, list):
+        return Vector3(target)
+    else:
+        return target.location
+
+def distance2D(targetObject, ourObject):
+    difference = toLocation(targetObject) - toLocation(ourObject)
+    return math.sqrt(difference.data[0]**2 + difference.data[1]**2)
