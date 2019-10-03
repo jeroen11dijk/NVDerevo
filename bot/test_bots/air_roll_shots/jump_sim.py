c664485023f97a87a0574c0080b936cd2152304c
Jeroen van Dijk <jeroen11dijk@gmail.com>
2019-10-03 22:37:27 +0200

    Added the batmobile hitbox and the ball object. Now includes intersection as well

--- bot/test_bots/air_roll_shots/jump_sim.py
+++ bot/test_bots/air_roll_shots/jump_sim.py
@@ -2,8 +2,8 @@ import sys
 from pathlib import Path
 
 sys.path.insert(1, str(Path(__file__).absolute().parent.parent.parent))
-from rlutilities.mechanics import Jump, Dodge
-from rlutilities.simulation import Car
+from rlutilities.mechanics import Dodge
+from rlutilities.simulation import Car, intersect, obb, Ball
 from rlutilities.linear_algebra import *
 
 # gravity
@@ -22,17 +22,35 @@ def get_height_at_time(t, d):
         return 0.5 * (-g) * ((t - d) ** 2) + a2 * (t - d) + c
     return 0
 
+
 car = Car()
 car.on_ground = True
 car.location = vec3(0.000000, -1252.049927, 18.639999)
 car.velocity = vec3(0, 1270, 0.45)
 car.rotation = mat3(-0.000000, -1.000000, -0.000000, 0.999982, -0.000000, 0.005944, -0.005944, -0.000000, 0.999982)
+
+ball = Ball()
+ball.location = vec3(0, 0, 234)
+ball.velocity = vec3(0, 0, 0)
+ball.angular_velocity = vec3(0, 0, 0)
+
 dodge = Dodge(car)
 dodge.duration = 0.9
 dodge.target = vec3(0, 0, 234)
-for i in range(13):
+
+batmobile = obb()
+batmobile.half_width = vec3(114.41, 42.335, 14.695)
+batmobile.center = car.location + dot(car.rotation, vec3(9.01, 0, 12.09))
+batmobile.orientation = car.rotation
+
+print(ball.hitbox().center)
+
+for i in range(600):
     dodge.step(1 / 60)
     car.step(dodge.controls, 1 / 60)
-    print(i * 1 / 60, car.location[2])
+    batmobile.center = car.location + dot(car.rotation, vec3(9.01, 0, 12.09))
+    batmobile.orientation = car.rotation
+    if intersect(ball.hitbox(), batmobile):
+        print(i/60)
 print(car.location)
 print(dodge.timer)
